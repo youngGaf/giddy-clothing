07e8fa8d6c79b15464fd789ee74efda8233acf06
youngGaf <youngabuhafiz@gmail.com>
2020-11-26 12:06:03 +0000

    using dynamic styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({title}) => (
-    <div className='menu-item'>
-        <div className='content'> 
-            <h1 className='title'>{ title }</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({title, imageUrl, size}) => (
+    <div 
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        className={`${size} menu-item`}>
+            <div className='content'> 
+                <h1 className='title'>{ title }</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
     </div>
 );
 export default MenuItem;
