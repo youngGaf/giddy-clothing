63c3e3c93de5a30b95f5d4ba891c8391d6cff415
youngGaf <youngabuhafiz@gmail.com>
2021-01-11 00:21:11 +0000

    chores: updated sigin component and google signin

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
+
 import './App.css';
 
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user })
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={ this.state.currentUser }/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  } 
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -45,10 +45,12 @@ class SignIn extends React.Component {
 							handleChange={this.handleChange} 
 							label='password'
 							required />
-						<CustomButton type='submit'>Sign in</CustomButton>
-						<CustomButton onClick={signInWithGoogle}> 
-							Sign in with google
-						</CustomButton>
+						<div className='buttons'>
+							<CustomButton type='submit'>Sign in</CustomButton>
+							<CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+								Sign in with google
+							</CustomButton>
+						</div>
 					</form>
 				</div>
 			)
